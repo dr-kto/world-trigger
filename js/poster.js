4c797bdcb7d8748f7769f3cfc63a40668d6724f0
Dias Bakhtiyarov <bakhtrv.dias@gmail.com>
2021-10-20 11:14:48 +0600

    developed pages

---
+++ js/poster.js
@@ -0,0 +1,19 @@
+// (function($){
+//     if ($('body#top').length) {
+//         $('html').addClass('pre');
+//       }
+//     $(window).on("load", function () {
+//         if ($('body#top').length) {
+//         setTimeout(function(){
+//             $('html').removeClass('pre');
+//             $('.main-img').removeClass('hide');
+
+//             if($('.movie-area .bg').css('display') !== 'none'){
+//             $('.main-img').find('[data-anim="fadein"]').attr({
+//                 'data-anim' : ''
+//             });
+//             }
+//         },1000);
+//         }
+//     });
+// })(jQuery);
